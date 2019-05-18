64575f9340dc66a8d4c716c1cb98778be8877daf
pencilCool <yhtangcoder@gmail.com>
2019-05-18 10:26:28 +0800

    init project

---
+++ MacQRCode/MacQRCode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacQRCode
+//
+//  Created by pencilCool on 2019/5/18.
+//  Copyright © 2019 pencilCool. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
